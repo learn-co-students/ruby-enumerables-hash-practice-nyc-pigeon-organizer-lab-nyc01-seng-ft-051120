e622dfc6e993abc67624e6cdc534b42e6d831cdf
Cody DeMartin <codythomasdemartin@gmail.com>
2020-04-09 17:29:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon = {}
+  
+  data.keys.each do |property| 
+    data[property].keys.each do |value|
+      data[property][value].each do |name|
+        pigeon[name] = pigeon[name] || {}  
+        pigeon[name][property] = pigeon[name][property] || []
+        pigeon[name][property] << value.to_s 
+      end
+    end
+  end
+ pigeon
+end
 end
