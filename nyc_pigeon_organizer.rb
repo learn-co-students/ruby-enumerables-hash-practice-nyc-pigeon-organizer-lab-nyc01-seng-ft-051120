93ecb6e07aedcdd90a97c9b1b9c8a361cecdd523
Eli Nass <eliftw@gmail.com>
2020-04-21 17:31:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organized_data = Hash.new
+
+  data.each { |pig_key,pig_val|
+    pig_val.each { |attribute_key,attribute_value|
+      attribute_value.each { |name|
+        if !organized_data.has_key?(name)
+          bird_template = Hash.new
+          data.keys.each{ |bird_attribute|
+            bird_template[bird_attribute] = Array.new
+          }
+          organized_data[name] = bird_template
+        end
+        organized_data[name][pig_key] << attribute_key.to_s
+      }
+    }
+  }
+
+
+  organized_data
 end
