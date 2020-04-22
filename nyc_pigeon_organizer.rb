20e72090cfab0f354b350826378528799c52a1f2
Austin Buhler <austinbuhler@gmail.com>
2020-04-22 14:58:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |color_gender_lives, value|
+    value.each do |attributes, array|
+      array.each do |name|
+        if new_hash[name] == nil
+          new_hash[name] = {}
+        end
+
+        if new_hash[name][color_gender_lives] == nil
+          new_hash[name][color_gender_lives] = []
+        end
+        new_hash[name][color_gender_lives] << attributes.to_s
+      end
+    end
+  end
+new_hash
 end
