43052e869154dd06e61c554f3f9385b277a0406e
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-26 18:18:31 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result=data.reduce({}) { |element, (key, value)|
+  value.count{|n|
+    element[n]=key
+  }
+    element
+    }
+  result
 end
