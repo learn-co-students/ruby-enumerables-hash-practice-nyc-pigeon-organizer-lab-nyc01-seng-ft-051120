cd887301457b1dc6c21160263dc4bcadb5658589
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-26 19:32:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,12 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
-  result=data.reduce({}) { |element, (key, value)|
-  value.count{|n|
-    element[n]=key
+  result=data.collect{ |element, (key, value)|
+  value.collect{ |element1, (key1, value1)|
+  value1.count{ |array_value|
+    element[:array_value]=value1
+  }
+element1
   }
     element
     }
