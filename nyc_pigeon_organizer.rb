2afb96d1ad845a6de22b876ed38f98776260c783
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-26 20:52:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,42 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
-  result=data.collect{ |element, (key, value)|
-  value.collect{ |element1, (key1, value1)|
-  value1.count{ |array_value|
-    element[:array_value]=value1
-  }
-element1
-  }
-    element
+  #result=data.collect{ |element, (key, value)|
+  #value.each{ |key1, value1)|
+
+  # value1.count{ |array_value|
+    # element[:array_value]=value1
+  # }
+  #
+  # }
+  #   element
+  #   }
+  # result
+result={}
+  data[:gender].each{ |key, value|
+  value.each{ |name|
+    result[name]={
+      :color =>[],
+      :gender =>[key.to_s],
+      :lives=>[]
     }
-  result
+
+  }
+
+  }
+  data[:color].each{ |key, value|
+    value.each{ |name|
+result[name][:color].push(key.to_s)
+
+    }
+
+  }
+data[:lives].each{ |key, value|
+  value.each{|name|
+    result[name][:lives].push(key.to_s)
+
+  }
+#binding.pry
+}
+result
 end
