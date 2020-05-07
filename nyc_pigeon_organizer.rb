7e191173e5c1b8da452dc807cb4b7ece87249e33
Chase <chaserabenn@gmail.com>
2020-05-07 18:34:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,55 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_data = {}
+
+  def get_names(data)
+    data[:color].reduce([]) do |memo, (k, v)|
+      memo << v
+      memo.flatten.uniq
+    end
+  end
+
+  names = get_names(data)
+
+  def add_names(data, new_data, names)
+    names.each do |bird|
+      new_data[bird] = {:color => [], :gender => [], :lives => []}
+    end
+  end
+
+  add_names(data, new_data, names)
+
+  def add_color(data, new_data, names)
+    new_data.each do |name, color|
+      data[:color].each do |color_name, name_array|
+        new_data[name][:color] << color_name.to_s if name_array.include?(name)
+      end
+    end
+  end
+
+  def add_gender(data, new_data, names)
+    new_data.each do |name, gender|
+      data[:gender].each do |gender_type, name_array|
+        new_data[name][:gender] << gender_type.to_s if name_array.include?(name)
+      end
+    end
+  end
+
+
+  def add_lives(data, new_data, names)
+    new_data.each do |name, lives|
+      data[:lives].each do |location, name_array|
+        new_data[name][:lives] << location.to_s if name_array.include?(name)
+      end
+    end
+  end
+
+
+  add_color(data, new_data, names)
+  add_gender(data, new_data, names)
+  add_lives(data, new_data, names)
+
+  new_data
 end
